b70625e81f2cc0b85c4f949bb376b3711b780d28
Anatolii <Parshykov>
2022-06-24 01:25:21 +0300

    .gitignore + new project

---
+++ AList/AList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
