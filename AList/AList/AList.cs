9fd79e6f870dd9a44380434794154bb15d9aabfc
Anatolii <Parshykov>
2022-06-25 16:23:58 +0300

    Search Max, GetEnum, indexing

--- AList/AList/AList.cs
+++ AList/AList/AList.cs
@@ -1,168 +1,174 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
+using System.Numerics;
 
 namespace AList
 {
-    public class AList<T> where T: IComparable
+    public class InterestingList<T> where T: INumber<T>
+
     {
-        private T[] _array;
-        private T[] _arrayMax;
-        private const int DefaultSize = 32;
-        private int _freePos = 0;
-        public int Length { get => _freePos; }
-        private T Max;
+    public T[] _array;
+    public IEnumerator GetEnumerator() => new InterestingListEnum<T>(_array, Length);
+    private const int DefaultSize = 32;
+    private int _freePos = 0;
+    private T _max;
+
+    public int Length { get => _freePos; }
+
+    public InterestingList()
+    {
+        _array = new T[DefaultSize];
+    }
+
+    public void Add(T element)
+    {
+        if (_freePos == _array.Length)
+            Expand();
+
+        _max = MaxValue(element, _max);
+
+        _array[_freePos] = element;
+        _freePos++;
+    }
+
+    public void AddAt(int pos, T element)
+    {
+        if (pos < 0 || pos >= Length)
+            throw new IndexOutOfRangeException($"Index: {pos}, Size: {Length}");
+
+        if (_freePos == _array.Length)
+            Expand();
         
-        public AList()
-        {
-            _array = new T[DefaultSize];
-            /*if (_array[0] is int || _array[0] is char || _array[0] is Enum)
-            {
-                throw new Exception();
-            }*/
-        }
+        _max = MaxValue(element, _max);
         
-        public void Add(T element)
-        {
-            if (_freePos == _array.Length)
-            {
-                Expand();
-            }
-            
-            if (element.CompareTo(Max) > 0)
-            {
-                Max = element;
-            }
-            
-            _array[_freePos] = element;
-            _arrayMax[_freePos] = Max;
+        _freePos++;
+        T[] newArray = new T[_array.Length];
+        newArray[pos] = element;
 
-            _freePos++;
-        }
-        
-        public void AddAt(int pos, T element)
-        {
-            if (pos < 0 || pos >= Length)
-                throw new IndexOutOfRangeException($"Index: {pos}, Size: {Length}");
+        for (int i = 0; i < pos; i++)
+            newArray[i] = _array[i];
 
-            if (_freePos == _array.Length)
-                Expand();
+        for (int i = pos; i < Length; i++)
+            newArray[i + 1] = _array[i];
 
-            _freePos++;
-            T[] newArray = new T[_array.Length];
-            newArray[pos] = element;
-            
-            for (int i = 0; i < pos; i++)
-                newArray[i] = _array[i];
+        _array = newArray;
+    }
 
-            for (int i = pos; i < Length; i++)
-                newArray[i + 1] = _array[i];
+    public void Remove(T element)
+    {
+        if (_freePos == 0)
+            throw new InvalidOperationException("Trying to remove an element from an empty interesting list");
 
-            _array = newArray;
-        }
-        
-        public void Remove(T element)
-        {
-            if (_freePos == 0)
-                throw new InvalidOperationException("Trying to remove an element from an empty alist");
+        int index = Array.IndexOf(_array, element, 0, Length);
 
-            int index = Array.IndexOf(_array, element, 0, Length);
+        if (index < 0)
+            return;
 
-            if (index < 0)
-                return;
-            
-            RemoveAt(index);
-            
-        }
-        
-        public void RemoveAt(int pos)
-        {
-            if (_freePos < pos)
-            {
-                throw new InvalidOperationException("Trying to remove an element on unknown index");
-            }
-            
-            _freePos--;
-            T[] newArray = new T[_array.Length];
+        RemoveAt(index);
+    }
 
-            for (int i = 0; i < pos; i++)
-                newArray[i] = _array[i];
+    public void RemoveAt(int pos)
+    {
+        if (_freePos <= pos)
+            throw new InvalidOperationException("Trying to remove an element on unknown index");
 
-            for (int i = pos; i < Length; i++)
-                newArray[i] = _array[i+1];
+        _freePos--;
+        T[] newArray = new T[_array.Length];
 
-            _array = newArray;
-        }
+        for (int i = 0; i < pos; i++)
+            newArray[i] = _array[i];
 
-        public T SearchMax()
-        {
-            return Max;
-        }
+        for (int i = pos; i < Length; i++)
+            newArray[i] = _array[i + 1];
 
-        private void Expand()
-        {
-            T[] newArray = new T[_array.Length * 2];
-            Array.Copy(_array, newArray, _array.Length);
-            _array = newArray;
-        }
+        _array = newArray;
+        SearchMax();
+    }
 
-        /*IEnumerator IEnumerable.GetEnumerator()
-        {
-            return (IEnumerator) GetEnumerator();
-        }
-        
-        public PeopleEnum GetEnumerator()
-        {
-            return new PeopleEnum(_people);
-        }*/
+    private void SearchMax()
+    {
+        if (_freePos == 0)
+            return;
+
+        _max = MinValue(_array[0], _max);
+
+        for (int i = 1; i < Length; i++)
+            _max = MaxValue(_array[i], _max);
+    }
+
+    private void Expand()
+    {
+        T[] newArray = new T[_array.Length * 2];
+        Array.Copy(_array, newArray, _array.Length);
+        _array = newArray;
+    }
+
+    private T MinValue(T element1, T element2) => element1 < element2 ? element1 : element2;
+    private T MaxValue(T element1, T element2) => element1 > element2 ? element1 : element2;
+
+    public T Max()
+    {
+        if (_freePos == 0)
+            throw new InvalidOperationException("Sequence contains no elements");
+
+        return _max;
     }
     
-    /*public class PeopleEnum : IEnumerator
+    public T this[int index]
     {
-        public Person[] _people;
-
-        // Enumerators are positioned before the first element
-        // until the first MoveNext() call.
-        int position = -1;
-
-        public PeopleEnum(Person[] list)
+        get
         {
-            _people = list;
+            if (index >= 0 && index < Length)
+                return _array[index];
+
+            throw new ArgumentOutOfRangeException
+                ("Index was out of range. Must be non-negative and less than the size of the collection. (Parameter 'index')");
+        }
+        set
+        { 
+            if (index >= 0 && index < Length)
+                _array[index] = value;
+            
+            throw new ArgumentOutOfRangeException
+                ("Index was out of range. Must be non-negative and less than the size of the collection. (Parameter 'index')");
+        } 
+    }
+    }
+
+    public class InterestingListEnum<T> : IEnumerator
+    {
+        private readonly T[] _array;
+        private readonly int _length;
+        
+        private int _position = -1;
+
+        public InterestingListEnum(T[] array, int length)
+        {
+            _array = array;
+            _length = length;
         }
 
         public bool MoveNext()
         {
-            position++;
-            return (position < _people.Length);
+            _position++;
+            return (_position < _length);
         }
 
-        public void Reset()
-        {
-            position = -1;
-        }
+        public void Reset() => _position = -1;
 
-        object IEnumerator.Current
+
+        object IEnumerator.Current { get => Current; }
+
+        public T Current
         {
-            get
+get
             {
-                return Current;
+                if (_position != -1 && _position < _length)
+                    return _array[_position];
+                
+                throw new ArgumentOutOfRangeException
+                    ("Index was out of range. Must be non-negative and less than the size of the collection. (Parameter 'index')");
             }
         }
-
-        public Person Current
-        {
-            get
-            {
-                try
-                {
-                    return _people[position];
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    throw new InvalidOperationException();
-                }
-            }
-        }
-    }*/
+    }
 }
--- AList/AList/Program.cs
+++ AList/AList/Program.cs
@@ -4,12 +4,4 @@ using System;
 using System.Numerics;
 
 
-T Add<T>(T lhs, T rhs)
-    where T : INumber<T>
-{
-    return lhs + rhs;
-}
-
-decimal d = 1*10;
-
-Console.WriteLine((int)d is int);
+Console.WriteLine("Hello world");
